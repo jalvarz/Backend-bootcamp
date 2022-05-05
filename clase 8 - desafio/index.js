307ea66f2ba7b3d7c12dde56d90279368dda8b46
jalvarz <java_banfield@hotmail.com>
2022-05-05 19:04:23 -0300

    clase 9

--- clase 8 - desafio/index.js
+++ clase 8 - desafio/index.js
@@ -1,1 +1,18 @@
-console.log("test")
+const express = require('express')
+const { Router } = express
+
+const app = express()
+const router = Router()
+
+router.get('/recurso', (req, res) => {
+   res.send('get ok')
+})
+
+router.post('/recurso', (req, res) => {
+   res.send('post ok')
+})
+
+app.use('/api', router)
+
+app.listen(8080)
+console.log("holis")
